8212311e3ee2c6d278e3162d676e9a70ad1296ab
Mohamed Aziz Haddadi <mohamedaziz.haddadi@insat.ucar.tn>
2026-02-12 13:23:31 +0100

    bb

--- obj/Debug/net8.0/Sayara.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sayara.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sayara")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0122302b4ca4fc337e3d981eea8bfa5c84588070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b704b2c103fe9ee411b5dbf567fdc2623680da33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sayara")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sayara")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
